3fe6e13e84e2ad22500df6df422473d1eca89d03
vamkim <36021127+vamkim@users.noreply.github.com>
2018-02-01 13:13:12 +0800

    New Item

---
+++ Hello-World/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Hello_World.Startup))]
+namespace Hello_World
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
